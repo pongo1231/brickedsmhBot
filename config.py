af75775ad450f05e99791175a0a8b6ba5809429c
pongo1231 <pongo1999712@gmail.com>
2016-08-28 09:38:24 +0200

    Fixed a variable set 2 times

--- config.py
+++ config.py
@@ -12,6 +12,4 @@ PASSWORD = 'PASS'
 
 SUBREDDIT = 'all' # The subreddit this bot should work at
 
-BOTMESSAGE = 'HELLO' # Text at the buttom of every message at
-
-BOTMESSAGE = '*****\n^This ^message ^was ^automated. ^/r/pongo1231 | ^[Source](https://github.com/pongo1231/brickedsmhBot)' # Message at the buttom of every message every message
+BOTMESSAGE = 'HELLO' # Text at the buttom of every message
